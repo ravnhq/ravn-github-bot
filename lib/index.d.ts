2c9ad71884a6976eba2efb6fa88703ba76f2c7ab
Donovan Hiland <donovan.hiland@gmail.com>
2020-01-17 17:19:56 -0700

    add a file

---
+++ lib/index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Application} app
+ */
+import { Application } from 'probot'
+declare const _default: (app: Application) => void
+export = _default
